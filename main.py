06f78884f5593e4c3dc503c6f793e66ba811a991
fxgang <fxg_ang@126.com>
2021-01-06 19:34:16 +0800

    数据测试

---
+++ main.py
@@ -0,0 +1,17 @@
+import sys
+# 程序入口
+print("hello word!", "nuances", 890, "23Jong", 'young')
+print("\nceshi测试中文输入", 100 + 580)
+#region
+age = 100
+desc = "这是一个人的年龄，OK"
+binfo = '就是这些聊'
+age = age+20
+result = '数据测试'
+users = "cesh zhogxin user"
+#endregion
+print(age, desc, binfo, "你看怎么样？")
+print("123", '')
+member = '{"name":"xiaoming", "gender": "male", "age": 18}'
+print(member)
+#TODO 明天開始正式聯繫Python基礎
