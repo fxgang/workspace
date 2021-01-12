79681eb1e1fbc9d38f01197438aacd7463cc760f
fxgang <fxg_ang@126.com>
2021-01-12 21:39:09 +0800

    简单模块

---
+++ smodule.py
@@ -0,0 +1,13 @@
+# _*_ coding: utf-8 _*_
+
+import smodulefunctioin
+smodulefunctioin.test()
+rst = smodulefunctioin.getusername("fxgang,opoi")
+print(rst)
+
+import sys
+rst = sys.path
+print(rst)
+
+import PIL
+print(PIL.__version__)
